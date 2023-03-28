49b9506bcf00af83f8eb8ae0087700300447c0ef
KenyanKidd <18257588078@139.com>
2023-03-28 10:20:47 +0800

    增加家庭成员类

---
+++ diet/src/main/java/com/example/diet/Domain/UsrFamily.java
@@ -0,0 +1,32 @@
+package com.example.diet.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsrFamily {
+    /**
+     * 过敏源list
+     */
+    private String[] allergens;
+    /**
+     * 疾病list
+     */
+    private long[] diseases;
+    /**
+     * 成员id
+     */
+    private Long familyid;
+    /**
+     * 成员关系
+     */
+    private String relation;
+    /**
+     * 对应用户id
+     */
+    private long usrid;
+
+}
