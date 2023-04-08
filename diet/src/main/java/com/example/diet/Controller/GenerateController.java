23097f8fa9b174d776ca73f3804aa7cd658348a7
KenyanKidd <18257588078@139.com>
2023-04-08 13:38:10 +0800

    更新生成controller

--- diet/src/main/java/com/example/diet/Controller/GenerateController.java
+++ diet/src/main/java/com/example/diet/Controller/GenerateController.java
@@ -3,6 +3,8 @@ package com.example.diet.Controller;
 import com.example.diet.Util.ChatGPTapiUtil;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import okhttp3.*;
+import okhttp3.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
@@ -11,8 +13,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/api/v1/generate")
@@ -22,7 +26,7 @@ public class GenerateController {
     private RestTemplate restTemplate;
 
     @RequestMapping("/plating")
-    public String PictureGenerate(@RequestParam("rec_name") String recname) {
+    public String PictureGenerate(@RequestParam("rec_name") String recname) throws IOException {
         /**
          * getForObject
          * 参数1 要请求的地址的url  必填项
@@ -30,34 +34,48 @@ public class GenerateController {
          * 参数3 请求携带参数 选填
          * getForObject 方法的返回值就是 被调用接口响应的数据
          */
+        Gson gson = new Gson();
         String url = "http://region-3.seetacloud.com:16682/sdapi/v1/txt2img";
         Map<String,String> paramMap = new HashMap<String, String>();
+        recname = "a photo of " + recname;
+        paramMap.put("prompt", recname);
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .connectTimeout(180, TimeUnit.SECONDS)
+                .readTimeout(180, TimeUnit.SECONDS)
+                .writeTimeout(180, TimeUnit.SECONDS)
+                .build();
+        String jsonString = gson.toJson(paramMap);
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), jsonString);
+        Request request = new Request.Builder()
+                .post(body)
+                .url(url)
+                .build();
+        Call call = client.newCall(request);
+
+        try (Response response = call.execute()) {
+            String result = response.body().string();
+            JsonObject obj = gson.fromJson(result, JsonObject.class);
+            result = obj.get("images").getAsString();
+            System.out.println(result);
+            return util.ImageUtil.generateImage(result,"D:\\Desktop\\");
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+
+
 //        paramMap.put("prompt", "Chinese Cuisine, Shredded Cucumber, with Exquisite plating, High color saturation, Realistic style, Overhead view");
-        paramMap.put("prompt", "a photo of tudousi");
         //先获取返回的字符串，若想获取属性，可以使用gson转化为实体后get方法获取
-        String result = restTemplate.postForObject(url, paramMap, String.class);
-        Gson gson = new Gson();
-        JsonObject obj = gson.fromJson(result, JsonObject.class);
-        result = obj.get("images").getAsString();
-
-        return util.ImageUtil.generateImage(result,"D:\\Desktop\\");
-        //2. getForEntity()
-        //获取实体ResponseEntity，可以用get函数获取相关信息
-//        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-//        System.out.println("responseEntity.getStatusCode() = " + responseEntity.getStatusCode());
-
-//        System.out.println("responseEntity.getStatusCodeValue() = " + responseEntity.getStatusCodeValue()); //responseEntity.getStatusCodeValue() = 200
-
-//        System.out.println("responseEntity.getBody() = " + responseEntity.getBody());   //responseEntity.getBody() = {"code":"0","data":{"address":"北京市海淀区","id":1,"password":"123456","role":0,"sex":0,"telephone":"10086","username":"小明"},"msg":"操作成功"}
-
-//        System.out.println("responseEntity.getHeaders() = " + responseEntity.getHeaders());//responseEntity.getHeaders() = [Content-Type:"application/json", Content-Length:"158", Server:"Werkzeug/0.14.1 Python/3.7.0", Date:"Sat, 16 Oct 2021 06:01:26 GMT"]
-
-//        System.out.println("responseEntity.getClass() = " + responseEntity.getClass());//responseEntity.getClass() = class org.springframework.http.ResponseEntity
-
+//        String result = restTemplate.postForObject(url, paramMap, String.class);
+//        Gson gson = new Gson();
+//        JsonObject obj = gson.fromJson(result, JsonObject.class);
+//        result = obj.get("images").getAsString();
+//
+//        return util.ImageUtil.generateImage(result,"D:\\Desktop\\");
     }
 
     @RequestMapping("/recipe")
-    public String RecipeGenerate(@RequestBody String[] ing_list) {
+    public String RecipeGenerate(@org.springframework.web.bind.annotation.RequestBody String[] ing_list) {
         StringBuilder query = new StringBuilder("我现在有以下食材：");
         for (String s : ing_list) {
             query.append(s).append("、");
